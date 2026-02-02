5d5204f29bc39d535aae6cd3353139272df851f1
Zaid Tariq <zaidt7497@gmail.com>
2026-02-02 22:32:32 +0500

    slick css

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import { nunitoRegular } from "@/fonts/fonts";
 import "./globals.css";
 import ProviderApp from "@/provider/ProviderApp";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 export default function RootLayout({
   children,
